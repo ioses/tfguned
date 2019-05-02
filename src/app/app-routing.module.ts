8f9757975427e459e5d62c02bc5ee3780efdd9ba
ioses <iosesgale@gmail.com>
2019-05-02 22:58:33 +0200

    Paginas añadidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'bluetooth', pathMatch: 'full' },
   { path: 'bluetooth', loadChildren: './pages/bluetooth/bluetooth.module#BluetoothPageModule' },
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
 ];
 
 @NgModule({
